aefb68a8302b21fd816d3cc55ea7d0a5db7a360c
Chinelo Osuji <139938233+osujc93@users.noreply.github.com>
2024-06-13 09:38:36 -0400

    Update superset_init.sh

--- superset_init.sh
+++ superset_init.sh
@@ -22,13 +22,22 @@ if [ "$1" = 'superset' ]; then
   superset load_examples
 
   echo "Adding PostgreSQL connection..."
-  cat <<EOF > /app/superset/datasource_config.yaml
+  datasource_config_path="/app/superset/datasource_config.yaml"
+  new_content=$(cat <<EOF
 databases:
 - database_name: Superset Postgres
   sqlalchemy_uri: postgresql+psycopg2://${AZURE_POSTGRES_USERNAME}:${AZURE_POSTGRES_PASSWORD}@${AZURE_POSTGRES_HOST}:${AZURE_POSTGRES_PORT}/${AZURE_SUPERSET_DB}
 EOF
+)
 
-  superset import-datasources --path /app/superset/datasource_config.yaml
+  if ! grep -q "${AZURE_POSTGRES_HOST}" "$datasource_config_path"; then
+    echo "$new_content" >> "$datasource_config_path"
+    echo "PostgreSQL connection added to datasource_config.yaml"
+  else
+    echo "PostgreSQL connection already exists in datasource_config.yaml"
+  fi
+
+  superset import-datasources --path "$datasource_config_path"
 
   echo "Starting the Superset server..."
   superset run -h 0.0.0.0 -p 8088 --with-threads --reload --debugger
