df891f8e12e0b4846f69761c79e110096308af95
Chinelo Osuji <139938233+osujc93@users.noreply.github.com>
2024-06-15 05:39:50 -0400

    Update main.tf

--- terraform-azure-vm-setup/main.tf
+++ terraform-azure-vm-setup/main.tf
@@ -68,7 +68,7 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_D5_v2"
+  vm_size               = "Standard_D2_v2" 
 
   storage_os_disk {
     name              = "myOsDisk"
